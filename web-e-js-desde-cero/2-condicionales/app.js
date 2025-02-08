56143383f70ffe5c451e18fa14110a5442c94905
Freida Portillo <freida.portillo@binmatter.com>
2025-02-07 19:45:52 -0700

    feat: add conditionals exercise

---
+++ web-e-js-desde-cero/2-condicionales/app.js
@@ -0,0 +1,110 @@
+let calificacion = prompt( "Ingresa la calificación" );
+
+calificacion = Number(calificacion);
+let mensaje = '';
+
+if(calificacion > 100 || calificacion < 0 || isNaN(calificacion)){
+    console.log('La calificación no es válida, debe ser entre 0 y 100');
+} else if(calificacion < 60){
+    console.log('No aprobaste');
+} else {
+    if(calificacion >= 90){
+        mensaje = 'Excelente';
+    } else if(calificacion >= 75 && calificacion <=89){
+        mensaje = 'Buena';
+    } else if(calificacion >= 60 && calificacion <=74){
+        mensaje = 'Suficiente';
+    }
+    
+    console.log(`Tu califación es: ${mensaje}`);
+}
+
+
+// ------ NOTAS DE CLASE ------
+/* var color = "Amarillo"
+var color = "Blue"
+
+console.log(color) */
+
+
+//let
+/*  let nombre = "carlos"
+nombre = "Pedro"
+
+//let nombre = "Luis" //incorrecto
+
+let fruta
+fruta = "piña"
+
+console.log(typeof fruta)  */
+
+//const
+
+/* const color = 'blanco'
+color = 'verde' // No es permitido
+
+console.log(color) */
+
+
+//Operadores matemáticos
+
+/* let shots = 2
+
+console.log( 6 + 4 + 23 )
+console.log("Tus shots son: " , shots)
+
+let msj = "Tus shots son: " + shots */
+
+/* let num1 = Number( prompt("Dame un número 1") ) 
+let num2 = Number( prompt("Dame un número 2") ) 
+console.log( num1 + num2 )*/
+
+//console.log(4 * "cinco") //NaN: Not a Number
+
+/* let shots = 3
+shots++
+shots++
+shots++
+shots += 3
+shots --
+shots --
+
+
+console.log(shots) */
+
+//Operadores de comparación
+
+// == : evalúa solo valor 
+// === : evalúa valor y tipo de dato (recomendable)
+
+//console.log( 3 !== "3" )
+
+/* let numerouno = prompt( "pon un numero" )
+let numerodos = 6
+
+console.log(numerouno === numerodos) */
+
+//Para entrar a VIP debes llamarte mario y tener más de 18 años
+/* let edad = 10
+let nombre = 'MaRiO'.toLowerCase()
+ */
+//.toLowerCase() : Transforma cadena de caractéres en minúscula
+//.toUpperCase() : Transforma cadena de caractéres en mayúscula
+
+//console.log( edad >= 18 || nombre === "mario")
+
+
+
+//Condicional if
+
+// let color = prompt("dime un color").toLowerCase()
+
+// if( color === "azul" ){
+//     console.log("Adivinaste el color! 😊")
+// }else if(color === "rojo"){
+//     console.log("Te ganaste una fresa")
+// }else if(color === "amarillo"){
+//     console.log("Te ganaste una piña")
+// }else{
+//     console.log("Perdiste :(")
+// }
