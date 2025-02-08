4554d51abf7d745fdcc64d7f9bcda8c708f673d9
Freida Portillo <freida.portillo@binmatter.com>
2025-02-07 19:14:06 -0700

    feat: add data type file

---
+++ web-e-js-desde-cero/tipos-de-datos.js
@@ -0,0 +1,18 @@
+console.log('42 es', typeof 42);
+console.log('Veinticinco es', typeof 'Veinticinco');
+console.log('-666 es', typeof -666);
+console.log('true es', typeof true);
+console.log('0 es', typeof 0);
+console.log("'' es", typeof '');
+console.log('null es', typeof null);
+console.log('undefined es', typeof undefined);
+console.log('FALSE es', typeof FALSE);
+
+// Custom
+console.log('3.14 es', typeof 3.14);  
+console.log('"Hola mundo" es', typeof "Hola mundo");  
+console.log('[] es', typeof []);  
+console.log('{} es', typeof {});  
+console.log('Symbol() es', typeof Symbol());  
+console.log('BigInt(9007199254740991) es', typeof BigInt(9007199254740991));  
+console.log('new Date() es', typeof new Date());    
