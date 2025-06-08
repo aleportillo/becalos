f23653c2bd38a5ad2bee1d6ad80b8f3653c57f5a
Freida Portillo <alejandrabypoca@gmail.com>
2025-06-08 01:22:52 -0600

    fix: project base

--- web-e-intro-react/01-intro-react/vite.config.ts
+++ web-e-intro-react/01-intro-react/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/becalos/web-e-intro-react/01-intro-react/dist/'
 })
