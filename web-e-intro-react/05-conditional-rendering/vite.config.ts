4118da3931fce169fe1873019aded1c809b0f150
Freida Portillo <alejandrabypoca@gmail.com>
2025-06-24 00:24:11 -0600

    fix: typo error

--- web-e-intro-react/05-conditional-rendering/vite.config.ts
+++ web-e-intro-react/05-conditional-rendering/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/becalos/web-e-intro-react/05-conditional-renderin/dist/'
+  base: '/becalos/web-e-intro-react/05-conditional-rendering/dist/'
 })
